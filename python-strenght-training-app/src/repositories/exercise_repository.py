b9228cefe0cb83e7764ae37b0df1e7cd17d3e5fa
Joni Joensuu <joni.joens@gmail.com>
2022-05-03 22:41:56 +0300

    test

--- python-strenght-training-app/src/repositories/exercise_repository.py
+++ python-strenght-training-app/src/repositories/exercise_repository.py
@@ -17,7 +17,7 @@ class ExerciseRepository:
             return False
 
     def list_exercises(self, date_id):
-        rows = self._cursor.execute('SELECT * FROM exercises WHERE day_id=(?)', [date_id])
+        rows = self._cursor.execute('SELECT * FROM exercises')
         return rows
 
     def delete_all(self, date_id):
