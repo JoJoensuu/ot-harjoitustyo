0b99f0c7b3b8fc58299f9eddca6510a0a90ebcbd
Joni Joensuu <joni.joens@gmail.com>
2022-05-03 16:48:18 +0300

    cleaned up redundant code

--- python-strenght-training-app/src/repositories/exercise_day_repository.py
+++ python-strenght-training-app/src/repositories/exercise_day_repository.py
@@ -3,36 +3,35 @@ from database_connection import get_database_connection
 class ExerciseDayRepository:
     def __init__(self, connection):
         self._connection = connection
+        self._cursor = self._connection.cursor()
 
     def add_day(self, day):
-        cursor = self._connection.cursor()
         if not self.check_date_exists(day):
-            cursor.execute('INSERT INTO exercise_days (date) VALUES (?)', [day])
+            self._cursor.execute('INSERT INTO exercise_days (date) VALUES (?)', [day])
             self._connection.commit()
             return True
         else:
             return False
 
     def list_days(self):
-        cursor = self._connection.cursor()
-        rows = cursor.execute("select * from exercise_days")
+        rows = self._cursor.execute("select * from exercise_days")
         return rows
 
     def check_date_exists(self, day):
-        cursor = self._connection.cursor()
-        cursor.execute('SELECT * FROM exercise_days WHERE date=(?)', [day])
-        result = cursor.fetchone()
+        self._cursor.execute('SELECT * FROM exercise_days WHERE date=(?)', [day])
+        result = self._cursor.fetchone()
         return result[1] if result else False
 
     def get_date_id(self, day):
-        cursor = self._connection.cursor()
-        cursor.execute('SELECT id FROM exercise_days WHERE date=(?)', [day])
-        result = cursor.fetchone()
+        self._cursor.execute('SELECT id FROM exercise_days WHERE date=(?)', [day])
+        result = self._cursor.fetchone()
         return result[0] if result else False
 
     def delete_all(self):
-        cursor = self._connection.cursor()
-        cursor.execute('delete from exercise_days')
+        self._cursor.execute('delete from exercise_days')
         self._connection.commit()
+
+    #def delete_single(self, id):
+
 
 exercise_day_repository = ExerciseDayRepository(get_database_connection())
