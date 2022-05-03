03ada77b5cd6bca6298a000c123f816c3f220b61
Joni Joensuu <joni.joens@gmail.com>
2022-05-03 14:01:54 +0300

    added variable to delete_all

--- python-strenght-training-app/src/tests/exercise_day_repository_test.py
+++ python-strenght-training-app/src/tests/exercise_day_repository_test.py
@@ -4,7 +4,7 @@ from repositories.exercise_day_repository import ExerciseDayRepository
 
 class TestExerciseDayRepository(unittest.TestCase):
     def setUp(self):
-        ExerciseDayRepository.delete_all()
+        ExerciseDayRepository.delete_all(self)
         self.date1 = datetime.datetime(2022,5,1)
         self.date2 = datetime.datetime(1990, 12, 1)
 
