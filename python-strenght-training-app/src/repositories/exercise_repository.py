3a5bb9bb2cc522a7c0d41c2db1a1ca6239d69c2f
Joni Joensuu <joni.joens@gmail.com>
2022-05-03 23:09:31 +0300

    cleaned up code

--- python-strenght-training-app/src/repositories/exercise_repository.py
+++ python-strenght-training-app/src/repositories/exercise_repository.py
@@ -8,8 +8,19 @@ class ExerciseRepository:
     def add_exercise(self, date_id, exercise):
         try:
             self._cursor.execute(
-                'INSERT INTO exercises (day_id, name, sets, reps, rest, comments) VALUES (?, ?, ?, ?, ?, ?)',
-                (date_id, exercise.name, exercise.sets, exercise.reps, exercise.rest, exercise.comments)
+                """
+                INSERT INTO exercises 
+                (day_id, name, sets, reps, rest, comments) 
+                VALUES (?, ?, ?, ?, ?, ?)"""
+                ,
+                (
+                    date_id,
+                    exercise.name,
+                    exercise.sets,
+                    exercise.reps,
+                    exercise.rest,
+                    exercise.comments
+                    )
             )
             self._connection.commit()
             return True
@@ -17,12 +28,16 @@ class ExerciseRepository:
             return False
 
     def list_exercises(self, date_id):
-        rows = self._cursor.execute('SELECT id, name, sets, reps, rest, comments FROM exercises where day_id=(?)', [date_id])
+        rows = self._cursor.execute("""
+        SELECT id, name, sets, reps, rest, comments 
+        FROM exercises where day_id=(?)""", [date_id])
         return rows
 
     def delete_all(self, date_id):
         try:
-            self._cursor.execute('DELETE FROM exercises WHERE day_id=(?)', [date_id])
+            self._cursor.execute("""
+            DELETE FROM exercises WHERE day_id=(?)
+            """, [date_id])
             self._connection.commit()
             return True
         except:
@@ -30,14 +45,18 @@ class ExerciseRepository:
 
     def delete_single(self, id):
         try:
-            self._cursor.execute('DELETE FROM exercises WHERE id=(?)', [id])
+            self._cursor.execute("""
+            DELETE FROM exercises WHERE id=(?)
+            """, [id])
             self._connection.commit()
             return True
         except:
             return False
 
     def get_exercise_data(self, id):
-        self._cursor.execute('SELECT * FROM exercises WHERE id=(?)', [id])
+        self._cursor.execute("""
+        SELECT * FROM exercises WHERE id=(?)
+        """, [id])
         result = self._cursor.fetchone()
         return result
 
