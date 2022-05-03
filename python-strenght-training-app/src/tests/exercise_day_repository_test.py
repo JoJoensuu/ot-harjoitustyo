299914828694e8994fe45d7a7e12b3f3464036fd
Joni Joensuu <joni.joens@gmail.com>
2022-05-03 14:05:44 +0300

    modified exercisedayrepository

--- python-strenght-training-app/src/tests/exercise_day_repository_test.py
+++ python-strenght-training-app/src/tests/exercise_day_repository_test.py
@@ -1,14 +1,14 @@
 import unittest
 import datetime
-from repositories.exercise_day_repository import ExerciseDayRepository
+from repositories.exercise_day_repository import exercise_day_repository
 
 class TestExerciseDayRepository(unittest.TestCase):
     def setUp(self):
-        ExerciseDayRepository.delete_all(self)
+        exercise_day_repository.delete_all()
         self.date1 = datetime.datetime(2022,5,1)
         self.date2 = datetime.datetime(1990, 12, 1)
 
     def test_add_day_adds_day_to_database(self):
-        ExerciseDayRepository.add_day(self.date1)
-        days = ExerciseDayRepository.list_days()
+        exercise_day_repository.add_day(self.date1)
+        days = exercise_day_repository.list_days()
         self.assertEqual(len(days), 1)
