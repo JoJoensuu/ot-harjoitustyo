b87aaabf4dcd258ffe3a765b0bcab58d4e4833ce
Joni Joensuu <joni.joens@gmail.com>
2022-05-15 15:27:35 +0300

    added new test modules

--- python-strenght-training-app/src/tests/conftest.py
+++ python-strenght-training-app/src/tests/conftest.py
@@ -1,5 +1,4 @@
-from initialize_database import initialize_database
-
+from initialize_test_database import initialize_database
 
 def pytest_configure():
     initialize_database()
---
+++ python-strenght-training-app/src/tests/initialize_test_database.py
@@ -0,0 +1,62 @@
+from test_database_connection import get_database_connection
+
+def drop_tables(connection):
+    """Removes existing tables from database.
+
+    Args:
+        connection: connection entity for database.
+    """
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        drop table if exists exercise_days;
+    ''')
+
+    cursor.execute('''
+        drop table if exists exercises;
+    ''')
+
+    connection.commit()
+
+def create_tables(connection):
+    """Creates tables into database
+
+    Args:
+        connection: connection entity for database.
+    """
+
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        create table exercise_days (
+            id integer primary key,
+            date date
+        );
+    ''')
+
+    cursor.execute('''
+        create table exercises (
+            id integer primary key,
+            day_id integer references exercise_days,
+            name text,
+            sets integer,
+            reps integer,
+            rest integer,
+            comments text
+        );
+    ''')
+
+    connection.commit()
+
+def initialize_database():
+    """Initializes database
+    """
+
+    connection = get_database_connection()
+
+    drop_tables(connection)
+    create_tables(connection)
+
+
+if __name__ == '__main__':
+    initialize_database()
